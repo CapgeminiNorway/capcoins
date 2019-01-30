5cb648c33ab95bdec9d24f9583979b345a5a7527
Yilmaz Guleryuz <397125+zeusbaba@users.noreply.github.com>
2019-01-30 14:02:36 +0100

    Update make-docker.sh

--- capcoins-api/make-docker.sh
+++ capcoins-api/make-docker.sh
@@ -6,6 +6,7 @@ echo "making build... linux"
 GOOS=linux GOARCH=amd64 go build capcoins-api.go
 
 echo "prepare env"
+# NB! replace this with your own docker hub username
 export dockerhubUser=zeusbaba
 export appName=capcoins-api
 export appVersion=1.0.0
@@ -14,8 +15,9 @@ export dockerImage=${dockerhubUser}/${appName}:${appVersion}
 echo "docker build for dockerImage: ${dockerImage}"
 docker build -t ${dockerImage} --rm --no-cache .
 
+# NB! uncomment this if you want the image to be published on docker
 #echo "docker push for dockerImage: ${dockerImage}"
 #docker push ${dockerImage}
 
-# to avoid name conflicts of builds for other envs
+# to avoid name conflicts of further builds for other envs
 mv capcoins-api capcoins-api_linux
